f9b5118cf4fa7fbd76d8e407341af7219818601b
daviesj70769 <78513526+daviesj70769@users.noreply.github.com>
2021-02-23 09:56:09 +1300

    Number improvments

---
+++ 03_Number_checker_v2.py
@@ -0,0 +1,36 @@
+#function goes here
+
+
+# Checks for an integer more than 0
+def int_check(question):
+
+    error = "please enter a whole number that is more than "
+
+    valid = False
+    while not valid:
+
+        # ask user for number and check it is valid
+        try:
+            response = int(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        # if an integer is not entered, display an error
+        except ValueError:
+            print(error)
+
+
+
+
+
+
+
+
+
+
+
+
+
